66b4146c213b15a6d14b437ac73bfe7a1c7b15ad
fiansyahm <fiansyahmm@gmail.com>
2024-05-21 22:30:19 +0700

    update footer

--- resources/views/template-user.blade.php
+++ resources/views/template-user.blade.php
@@ -109,10 +109,10 @@
         <div class="row justify-content-center">
           <div class="col-lg-6">
             <!-- <h4>Join Our Newsletter</h4> -->
-            <!-- <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe"> -->
-            </form>
+            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+            {{-- <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form> --}}
           </div>
         </div>
       </div>
@@ -122,7 +122,7 @@
       <div class="container">
         <div class="row">
 
-          <div class="col-lg-3 col-md-9 footer-contact">
+          <div class="col-md-9">
             <h3>Sistem Informasi Penatausahaan Aset<span></span></h3>
             <p>
               A108 Adam Street <br>
@@ -133,40 +133,16 @@
             </p>
           </div>
 
-          <div class="col-lg-3 col-md-3 footer-links">
+          <div class="col-md-3">
             <h4>Useful Links</h4>
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="/">Beranda</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="/asset/list/all">Ruang Aset</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="/asset/map/all">Ruang Peta</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="/wpadmin">Dashboard Admin</a></li>
             </ul>
           </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <!-- <h4>Our Services</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li> -->
-            </ul>
-          </div>
-
-          <!-- <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Social Networks</h4>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
-            <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-            </div>
-          </div> -->
-
+          
         </div>
       </div>
     </div>
